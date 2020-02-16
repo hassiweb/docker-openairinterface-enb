667031822b76eaa81293490bfc1653efcc7ed53d
hassiweb <nryk.hashimoto@gmail.com>
2020-02-17 01:19:01 +0900

    Support T-Tracer

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -e
 
 OAI_VER=v1.0.3
+#OAI_VER=v1.2.1
 
 usage() {
-  echo "Usage: sh build.sh {enb lmssdr|rcc nfapi|rru nfapi lmssdr}"
+  echo "Usage: sh build.sh {enb|split}"
 }
 
 main() {
@@ -16,9 +17,9 @@ main() {
     docker build -f Dockerfile.base -t hassiweb/oai-base:$OAI_VER . --build-arg OAI_VER=$OAI_VER
     docker build -f Dockerfile.rcc -t hassiweb/oaircc:$OAI_VER . --build-arg OAI_VER=$OAI_VER
     docker build -f Dockerfile.rru.lmssdr -t hassiweb/oairru-lms:$OAI_VER . --build-arg OAI_VER=$OAI_VER
-  elif [ $1 = "ue" -a $2 = "nfapi" ]; then
-    docker build -f Dockerfile.base -t hassiweb/oai-base:$OAI_VER . --build-arg OAI_VER=$OAI_VER
-    docker build -f Dockerfile.ue.nfapi -t hassiweb/oaiue-nfapi:$OAI_VER . --build-arg OAI_VER=$OAI_VER
+#  elif [ $1 = "ue" -a $2 = "nfapi" ]; then
+#    docker build -f Dockerfile.base -t hassiweb/oai-base:$OAI_VER . --build-arg OAI_VER=$OAI_VER
+#    docker build -f Dockerfile.ue.nfapi -t hassiweb/oaiue-nfapi:$OAI_VER . --build-arg OAI_VER=$OAI_VER
   else
     usage
   fi
