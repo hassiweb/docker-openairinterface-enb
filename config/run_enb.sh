4fef19feeb9729a32c2f820b14b3bb158e737159
hassiweb <nryk.hashimoto@gmail.com>
2020-03-16 03:30:51 +0900

    Add telnet server mode

--- config/run_enb.sh
+++ config/run_enb.sh
@@ -3,6 +3,7 @@
 RF_CONF="/etc/limemicro/trx-lms7002m/config-limeSDR/LimeSDR_Mini_above_1p8GHz.ini"
 #ENB_CONF="/etc/openairinterface/enb.band1.tm1.50PRB.lmssdr.conf"
 ENB_CONF="/etc/openairinterface/enb.conf"
+#OPTION="--telnetsrv"
 LOG_DIR="/var/log/openairinterface"
 LOG_FILE="${LOG_DIR}/enb.log"
 
@@ -13,4 +14,4 @@ touch $LOG_FILE
 
 tail -f $LOG_FILE &
 
-lte-softmodem -O $ENB_CONF --rf-config-file $RF_CONF --nbiot-disable > $LOG_FILE 2>&1
+lte-softmodem -O $ENB_CONF --rf-config-file $RF_CONF $OPTION --nbiot-disable > $LOG_FILE 2>&1
